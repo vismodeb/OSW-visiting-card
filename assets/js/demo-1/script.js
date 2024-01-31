a8268683718f4a4f30c5d13d55044e76a2fc8217
vismodev <vismodeb2000@gmail.com>
2024-01-31 16:30:33 +0600

    testimonial slider add

--- assets/js/demo-1/script.js
+++ assets/js/demo-1/script.js
@@ -12,3 +12,16 @@ $('[data-fancybox="gallery"]').fancybox({
   loop: false,
   protect: true
 });
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
